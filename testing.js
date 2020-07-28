bad492646da8ba74625576b78f61648ed3a356e2
ManRoom <purplegusik@gmail.com>
2020-07-28 10:12:08 +0300

    new

---
+++ testing.js
@@ -0,0 +1,18 @@
+`var str = "qwe";
+//console.log(str.substr(1, 1));
+
+
+var str = 'HTTP://WWW.YANDEX.RU';
+
+//if(str.indexOf('yandex.ru') + 1) {
+//}
+//console.log(str.toLowerCase());`
+
+
+
+
+function sayHi() {
+    console.log('Привет');
+  }
+  
+  setTimeout(sayHi, 2000);
